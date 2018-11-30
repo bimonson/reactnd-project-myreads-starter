797c16408b030e4bac64bd6c6f293f75a348bbed
Benjamin Simonson <bfsimon@gmail.com>
2018-11-30 15:27:31 -0500

    turn search link into Link component

--- src/components/BookCase.js
+++ src/components/BookCase.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
 class BookCase extends Component {
@@ -41,7 +42,9 @@ class BookCase extends Component {
             </div>
           </div>
           <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            <Link
+              to="/search"
+              >Add a book</Link>
           </div>
         </div>
     )
